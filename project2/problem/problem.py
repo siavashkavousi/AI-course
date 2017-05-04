5e44f5fda9f035e7c221a8a5093c9322870456c2
siavash <siavash.kavousi.sk@gmail.com>
2017-05-04 15:14:33 +0430

    project2 initial commit

---
+++ project2/problem/problem.py
@@ -0,0 +1,49 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Problem(object):
+    __metaclass__ = ABCMeta
+
+    @property
+    @abstractmethod
+    def init_node(self):
+        pass
+
+    @init_node.setter
+    @abstractmethod
+    def init_node(self, node):
+        pass
+
+    @abstractmethod
+    def actions(self, node):
+        pass
+
+    @abstractmethod
+    def result(self, action, node):
+        pass
+
+    @abstractmethod
+    def is_goal(self, node):
+        pass
+
+    @abstractmethod
+    def compute_cost(self, current_node, parent_node):
+        pass
+
+    @property
+    @abstractmethod
+    def goal_node(self):
+        pass
+
+    @goal_node.setter
+    @abstractmethod
+    def goal_node(self, node):
+        pass
+
+    @abstractmethod
+    def solution(self, goal_node):
+        pass
+
+    @abstractmethod
+    def get_h(self, node):
+        pass
