de55b4c7896264536581656ea20c712932cdb8a0
siavash <siavash.kavousi.sk@gmail.com>
2017-05-21 23:40:22 +0430

    Adds cooling schedulers

--- project2/solver/anneal.py
+++ project2/solver/anneal.py
@@ -1,18 +1,20 @@
 import math
+from abc import ABCMeta, abstractmethod
+from random import random, choice
 
 from problem.problem import Problem
 from solver.solver import Solver
-from random import random, choice
 
 
 class Annealer(Solver):
-    def __init__(self, problem: Problem):
+    def __init__(self, problem: Problem, cooling_scheduler: CoolingScheduler, temperature=5000, threshold=1):
         super().__init__(problem)
-        self.temperature = 5000
-        self.threshold = 1
-        self.cooling_rate = 0.05
+        self.temperature = temperature
+        self.threshold = threshold
+        self.cooling_scheduler = cooling_scheduler
 
     def solve(self):
+        n = 0
         node = self.problem.init_node
 
         while self.temperature > self.threshold:
@@ -25,7 +27,8 @@ class Annealer(Solver):
             if self.problem.compute_cost(node) < self.problem.compute_cost(self.problem.best_node):
                 self.problem.best_node = node
 
-            self.temperature = 1 - self.cooling_rate
+            n += 1
+            self.temperature = self.cooling_scheduler.tn()
 
         self.problem.solution()
 
@@ -33,3 +36,35 @@ class Annealer(Solver):
         if new_energy < current_energy:
             return 1
         return math.exp((current_energy - new_energy) / self.temperature)
+
+
+class CoolingScheduler(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, t0, a=0.8):
+        self.t0 = t0
+        self.a = a
+
+    @abstractmethod
+    def tn(self, n):
+        pass
+
+
+class ExponentialMultiplicativeScheduler(CoolingScheduler):
+    def tn(self, n):
+        return self.t0 - math.pow(self.a, n)
+
+
+class LogarithmicMultiplicativeScheduler(CoolingScheduler):
+    def tn(self, n):
+        return self.t0 / (1 + self.a * math.log(1 + n))
+
+
+class LinearMultiplicativeScheduler(CoolingScheduler):
+    def tn(self, n):
+        return self.t0 / (1 + self.a * n)
+
+
+class QuadraticMultiplicativeScheduler(CoolingScheduler):
+    def tn(self, n):
+        return self.t0 / (1 + self.a * math.pow(n, 2))
--- project2/test/traveling_salesman.py
+++ project2/test/traveling_salesman.py
@@ -1,10 +1,10 @@
 from problem.traveling_salesman import TravelingSalesMan
-from solver.anneal import Annealer
+from solver.anneal import Annealer, ExponentialMultiplicativeScheduler
 from solver.hill_climbing import HillClimbing, Mode
 
 if __name__ == '__main__':
     traveling_sales_man = TravelingSalesMan()
-    annealer = Annealer(traveling_sales_man)
+    annealer = Annealer(traveling_sales_man, ExponentialMultiplicativeScheduler(5000))
     hill_climbing = HillClimbing(traveling_sales_man, mode=Mode.SIMPLE)
     annealer.solve()
     hill_climbing.solve()
